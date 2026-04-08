9816e35384d471216097ed8efa9aa818999566df
demeter-app <software.demeter@gmail.com>
2026-04-08 23:59:33 +0200

    v7

--- sw.js
+++ sw.js
@@ -1,48 +1,50 @@
-const CACHE_NAME = 'demeter-v1';
+const CACHE_NAME = 'trec-v1';
 const ASSETS = [
-'/',
-'/index.html',
-'/manifest.json',
-'/icon-192.png',
-'/icon-512.png'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
 self.addEventListener('install', function(e) {
-e.waitUntil(
-caches.open(CACHE_NAME).then(function(cache) {
-return cache.addAll(ASSETS);
-})
-);
-self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', function(e) {
-e.waitUntil(
-caches.keys().then(function(keys) {
-return Promise.all(
-keys.filter(function(k) { return k !== CACHE_NAME; })
-.map(function(k) { return caches.delete(k); })
-);
-})
-);
-self.clients.claim();
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE_NAME; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', function(e) {
-if (e.request.url.indexOf('supabase.co') !== -1 ||
-e.request.url.indexOf('googleapis') !== -1 ||
-e.request.url.indexOf('jsdelivr') !== -1 ||
-e.request.url.indexOf('cloudflare') !== -1) {
-e.respondWith(
-fetch(e.request).catch(function() {
-return caches.match(e.request);
-})
-);
-} else {
-e.respondWith(
-caches.match(e.request).then(function(cached) {
-return cached || fetch(e.request);
-})
-);
-}
+  var url = e.request.url;
+  
+  // Laisser passer toutes les requetes externes sans les toucher
+  if (url.indexOf('supabase.co') !== -1 ||
+      url.indexOf('googleapis') !== -1 ||
+      url.indexOf('jsdelivr') !== -1 ||
+      url.indexOf('cloudflare') !== -1 ||
+      url.indexOf('gstatic') !== -1 ||
+      url.indexOf('fonts') !== -1) {
+    return;
+  }
+
+  // Cache uniquement les assets locaux
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request);
+    })
+  );
 });
